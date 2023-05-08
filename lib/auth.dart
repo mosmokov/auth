5221ba4a8940a23865187d3ea0c4694938ae81e9
mosmokov <mosmokov@gmail.com>
2023-05-08 15:23:43 +0300

    AUTH

---
+++ lib/auth.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:conduit/conduit.dart';
+
+class AppService extends ApplicationChannel{
+  late final ManagedContext managedContext;
+
+  @override
+  Future prepare() {
+    final persistentStore = _initDataBase();
+    managedContext = ManagedContext(ManagedDataModel.fromCurrentMirrorSystem(), persistentStore);
+    return super.prepare();
+  }
+
+  @override
+  Controller get entryPoint => Router();
+  
+  PostgreSQLPersistentStore _initDataBase() {
+    final username = Platform.environment["DB_USERNAME"] ?? "admin";
+    final password = Platform.environment["DB_PASSWORD"] ?? "root";
+    final host = Platform.environment["DB_HOST"] ?? "127.0.0.1";
+    final port = int.parse(Platform.environment["DB_PORT"] ?? "5432");
+    final databaseName = Platform.environment["DB_NAME"] ?? "postgres";
+    return PostgreSQLPersistentStore(username, password, host, port, databaseName);
+  }
+}
